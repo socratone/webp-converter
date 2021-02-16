434febf9050d4c3c1b522bdaa429444c0adf6d12
socratone <gim2origin@gmail.com>
2021-02-16 11:14:24 +0900

    Tab navigation 기능 구현

--- index.js
+++ index.js
@@ -5,17 +5,7 @@ const app = new Vue({
   },
   methods: {
     select: function (tab) {
-      console.log('tab:', tab)
-      console.log('this.tab:', this.tab)
       this.tab = tab;
     }
   }
 });
-
-
-// const app = new Vue({
-//   el: '#app',
-//   data: {
-//     message: 'Hello Vue!'
-//   }
-// });
